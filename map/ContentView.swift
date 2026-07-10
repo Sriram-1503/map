ab8c8345dc3b5cc4ddcef3ceba42ce72715f590f
sri ram <sriramareddy1503@gmail.com>
2026-07-10 17:38:39 +0530

    Initial Commit

---
+++ map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  map
+//
+//  Created by sunny on 10/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by sunny on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
